07443b11ab68d669678834f97402cee093c5ee5f
wiib <16376176+wiib@users.noreply.github.com>
2022-07-29 17:31:20 -0500

    feat(command): Add ping command

---
+++ src/commands/ping.ts
@@ -0,0 +1,19 @@
+import dedent from "ts-dedent";
+import { Command } from "../common/types";
+import { SlashCommandBuilder } from "discord.js";
+import type { CommandInteraction } from "discord.js";
+
+export default class PingCommand implements Command {
+    public data = new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Ping the bot to see if it is alive.");
+
+    public async execute(interaction: CommandInteraction) {
+        const reply = await interaction.reply({ content: "Pinging...", fetchReply: true });
+        const content = dedent`...Pong!
+        Bot latency: ${Math.round(interaction.client.ws.ping)}ms.
+        API Latency: ${reply.createdTimestamp - interaction.createdTimestamp}ms.`;
+
+        await interaction.editReply(content);
+    }
+}

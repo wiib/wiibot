f2c538afa25b8097955f4ac9a8e374ab49b2b22d
wiib <16376176+wiib@users.noreply.github.com>
2022-07-30 23:09:51 -0500

    refactor: Implement recursive readdirSync in main command logic

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import "./lib/setup";
-import { readdirSync } from "node:fs";
 import { join } from "node:path";
 import { Client, Collection, GatewayIntentBits } from "discord.js";
-import { Command } from "./common/types";
+import {readdirSyncRecursive} from "./common/utils";
+import type { Command } from "./common/types";
 
 // Initialize client
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -10,12 +10,12 @@ const commands = new Collection<string, Command>();
 
 // Read command files
 const commandsPath = join(__dirname, "commands");
-const commandFiles = readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+const commandFiles = readdirSyncRecursive(commandsPath).filter((file) => file.endsWith(".js"));
 
 // Store command classes in collection
 for (const file of commandFiles) {
-    const filePath = join(commandsPath, file);
-    const f = require(filePath);
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const f = require(file);
     const command: Command = new f.default();
     commands.set(command.data.name, command);
 }
